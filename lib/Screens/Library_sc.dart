7b561d17738dfcc6a3a4902abecff7faa55eb04c
Sanjay-2000 <sanjay.space.66@gmail.com>
2021-04-15 18:14:51 +0530

    intial commit

--- lib/Screens/Library_sc.dart
+++ lib/Screens/Library_sc.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:spotify_ui/Constants.dart';
 import 'package:spotify_ui/main.dart';
 
 class LibraryScreen extends StatefulWidget {
@@ -10,77 +11,108 @@ class LibraryScreen extends StatefulWidget {
 class _LibraryScreenState extends State<LibraryScreen> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      initialIndex: 0,
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          bottom: TabBar(
-            dragStartBehavior: DragStartBehavior.start,
-            isScrollable: true,
-            tabs: [
-              Tab(
-                text: 'chats',
-              ),
-              Tab(
-                text: 'chats',
-              ),
-              Tab(
-                text: 'chats',
-              ),
+    return Container(
+      color: Colors.black,
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            toolbarHeight: kToolbarHeight,
+            bottom: TabBar(
+              unselectedLabelStyle: TextStyle(fontSize: 70),
+              indicatorColor: Colors.transparent,
+              tabs: [
+                Tab(
+                  child: Text(
+                    'Music',
+                    style: kprimarytextstyle,
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'Podcast',
+                    style: kprimarytextstyle,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Music(),
+              Container(
+                child: Column(
+                  children: [
+                    ListTile(
+                      leading: Image(
+                        image: AssetImage('images/b4.jpg'),
+                      ),
+                      title: Text(
+                        'Muscician',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+              // Icon(Icons.directions_transit),
+              //Icon(Icons.directions_bike),
             ],
           ),
-        ),
-        body: TabBarView(
-          children: [
-            Chats(),
-            Icon(Icons.directions_transit),
-            Icon(Icons.directions_bike),
-          ],
         ),
       ),
     );
   }
 }
 
-class Chats extends StatelessWidget {
+class Music extends StatefulWidget {
+  @override
+  _MusicState createState() => _MusicState();
+}
+
+class _MusicState extends State<Music> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.teal,
-      height: 200,
-      child: Container(
-        padding: EdgeInsets.all(0),
-        child: DefaultTabController(
-          length: 3,
-          child: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.white,
-              bottom: TabBar(
-                dragStartBehavior: DragStartBehavior.start,
-                isScrollable: true,
-                tabs: [
-                  Tab(
-                    text: 'chats',
-                  ),
-                  Tab(
-                    text: 'chats',
-                  ),
-                  Tab(
-                    text: 'chats',
-                  ),
-                ],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          toolbarHeight: kToolbarHeight,
+          bottom: TabBar(
+            indicatorColor: Colors.greenAccent[400],
+            tabs: [
+              Tab(
+                child: Text(
+                  'Music',
+                  style: ksecondarytextstyle,
+                ),
               ),
-            ),
-            body: TabBarView(
-              children: [
-                Icon(Icons.directions_transit),
-                Icon(Icons.directions_transit),
-                Icon(Icons.directions_bike),
-              ],
-            ),
+              Tab(
+                child: Text(
+                  'Podcast',
+                  style: ksecondarytextstyle,
+                ),
+              ),
+              Tab(
+                child: Text(
+                  'album',
+                  style: ksecondarytextstyle,
+                ),
+              ),
+            ],
           ),
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.directions_transit),
+            Icon(Icons.directions_bike),
+            Icon(Icons.directions_transit),
+          ],
         ),
       ),
     );
