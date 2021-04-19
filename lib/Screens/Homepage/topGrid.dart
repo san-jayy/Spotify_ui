3cad0a15084aa6562dce970ed2495fe5b571c4fd
Sanjay-2000 <sanjay.space.66@gmail.com>
2021-04-19 10:20:35 +0530

    need to be changed

--- lib/Screens/Homepage/topGrid.dart
+++ lib/Screens/Homepage/topGrid.dart
@@ -7,32 +7,28 @@ class Tophomegrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(5.0),
-      child: Container(
-        child: Center(
-          child: Container(
-              height: 60,
-              width: 176,
-              decoration: BoxDecoration(
-                  color: Colors.grey[800],
-                  borderRadius: BorderRadius.circular(5),
-                  image: DecorationImage(
-                      image: AssetImage(image),
-                      alignment: Alignment.centerLeft)),
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    top: 10, right: 20, left: 60, bottom: 10),
-                child: Center(
-                  child: Text(
-                    text,
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
-                  ),
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Container(
+            width: 176,
+            decoration: BoxDecoration(
+                color: Colors.grey[800],
+                borderRadius: BorderRadius.circular(5),
+                image: DecorationImage(
+                    image: AssetImage(image), alignment: Alignment.centerLeft)),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 10, right: 20, left: 60, bottom: 10),
+              child: Center(
+                child: Text(
+                  text,
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
                 ),
-              )),
-        ),
+              ),
+            )),
       ),
     );
   }
